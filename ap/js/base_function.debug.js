7f8a931508c77f2ca9246a2d4fbc25c61086f064
younsang <younsang.jo@gmail.com>
2017-11-21 16:29:20 +0900

    v2.6.17 - ap 2017/11/21 이미지 추가

--- ap/js/base_function.debug.js
+++ ap/js/base_function.debug.js
@@ -1,26 +1,26 @@
-var LayerPopup;
+/****************
+ * Common Class *
+ ****************/
 
-/**
- * VendorDevice Class
- */
-
-/**
- * LineTab Class
- */
-
-/**
- * SelectBox Class
- */
-
-/**
- * CalendarPopup Class
- */
-
-/**
- * showHide Class ?
- */
+var LayerPopup, CalendarPopup;
 
 $(function(){
+
+  /**
+   * VendorDevice Class
+   */
+
+  /**
+   * LineTab Class
+   */
+
+  /**
+   * SelectBox Class
+   */
+
+  /**
+   * showHide Class ?
+   */
 
   /**
    * LayerPopup Class
@@ -32,6 +32,7 @@ $(function(){
 
     this.openPopup = function( $popupName ){
 
+      $('html, body').addClass('open-layer');
       $layerWrap.addClass('on');
       $popupName.addClass('on');
 
@@ -39,6 +40,7 @@ $(function(){
 
     this.closePopup = function( $popupName ){
 
+      $('html, body').removeClass('open-layer');
       $layerWrap.removeClass('on');
       $popupName.removeClass('on');
 
@@ -51,10 +53,61 @@ $(function(){
 
     }
 
+  };
+
+  /**
+   * CalendarPopup Class
+   */
+
+  CalendarPopup = new function(){
+
+    // call jquery-ui api when loading
+    $('.timesetting-date-day').datepicker({
+      showMonthAfterYear : true,
+      showOtherMonths : true,
+      selectOtherMonths : true,
+
+      prevText : 'p',
+      nextText : 'n',
+      dayNames : ['월', '화', '수', '목', '금', '토', '일' ],
+      dayNamesMin : ['월', '화', '수', '목', '금', '토', '일' ],
+      dayNamesShort : ['월', '화', '수', '목', '금', '토', '일' ],
+      monthNames : ['. 1', '. 2', '. 3', '. 4', '. 5', '. 6', '. 7', '. 8', '. 9', '. 10', '. 11', '. 12']
+    });
+
+    this.showDatePicker = function(type){
+
+      $('.timesetting-date-day').datepicker(type);
+
+    }
 
   };
 
 });
+
+
+
+/********************
+ * Common Execution *
+ ********************/
+
+$(function(){
+
+  // date picker 열기
+  $('.timesetting-date-calendar').on('click', function(){
+
+    CalendarPopup.showDatePicker('show');
+
+  });
+
+});
+
+
+
+/**************
+ * Base Class *
+ **************/
+
 var TableLike;
 
 $(function(){
@@ -288,6 +341,13 @@ $(function(){
   };
 
 });
+
+
+
+/******************
+ * Base Execution *
+ ******************/
+
 $(function(){
 
   /**
@@ -379,3 +439,5 @@ $(function(){
   });
 
 });
+
+
